869d499f15853cea9f0ae3b29959e8fb27ce4ed8
Ollie O'Donnell <oliverodaa@gmail.com>
2017-04-22 20:28:20 -0700

    Saturday progress

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -38,11 +38,7 @@ container.appendChild(renderer.domElement);
 // create a point light
 var pointLight =
   new THREE.PointLight(0xFFFFFF);
-
-// set its position
-pointLight.position.x = 0;
-pointLight.position.y = 2.5;
-pointLight.position.z = -30;
+pointLight.position.set( 0, 1.5, -20 );
 
 // add to the scene
 scene.add(pointLight);
@@ -73,46 +69,51 @@ var sphere = new THREE.Mesh(
 
 // Move the Sphere back in Z so we
 // can see it.
-sphere.position.z = -30;
+sphere.position.z = -15;
 
 // Finally, add the sphere to the scene.
 scene.add(sphere);
 
+
+// // wireframe
+// var geo = new THREE.EdgesGeometry( sphere.geometry ); // or WireframeGeometry
+// var mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+// var wireframe = new THREE.LineSegments( geo, mat );
+// sphere.add( wireframe );
+
 function addPlane(width, height, color, x, y, z, rotX, rotY, rotZ) {
   var geometry = new THREE.PlaneGeometry( width, height);
-  var material = new THREE.MeshLambertMaterial( {color: color, side: THREE.DoubleSide} );
+  var material = new THREE.MeshPhongMaterial( {color: color, side: THREE.DoubleSide} );
   var wall = new THREE.Mesh( geometry, material );
   scene.add( wall );
 
-  if (x != null) {
-    wall.position.x = x;    
+  if (x !== null) {
+    wall.position.x = x;
   }
-  if (y != null) {
-    wall.position.y = y;    
+  if (y !== null) {
+    wall.position.y = y;
   }
-  if (z != null) {
+  if (z !== null) {
     wall.position.z = z;
   }
-  if (rotX != null) {
-    wall.rotation.x = rotX;    
+  if (rotX !== null) {
+    wall.rotation.x = rotX;
   }
-  if (rotY != null) {
-    wall.rotation.y = rotY;    
+  if (rotY !== null) {
+    wall.rotation.y = rotY;
   }
-  if (rotZ != null) {
+  if (rotZ !== null) {
     wall.rotation.z = rotZ;
   }
+  var geo = new THREE.EdgesGeometry( wall.geometry ); // or WireframeGeometry
+  var mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+  var wireframe = new THREE.LineSegments( geo, mat );
+  wall.add( wireframe );
 }
 
-addPlane(20, 2.5, 0xffff00, 2.5, null, -30, null, Math.PI/2, null);
-addPlane(20, 2.5, 0xffff00, -2.5, null, -30, null, Math.PI/2, null);
-addPlane(5, 20, 0xffff00, -2.5, null, -30, null, null, null);
-
-// wireframe
-var geo = new THREE.EdgesGeometry( sphere.geometry ); // or WireframeGeometry
-var mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
-var wireframe = new THREE.LineSegments( geo, mat );
-sphere.add( wireframe );
+addPlane(20, 2.5, 0xa03013, 2.5, null, -15, null, Math.PI/2, null);
+addPlane(20, 2.5, 0x90302F, -2.5, null, -15, null, Math.PI/2, null);
+addPlane(5, 20, 0xf0303D, null, -1.25, -15, Math.PI/2, null, null);
 
 // Draw!
 renderer.render(scene, camera);
