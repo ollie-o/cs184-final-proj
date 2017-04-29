a444d51ae1f495c1b1e65eb94f5da18a092ffd9a
Ollie O'Donnell <oliverodaa@gmail.com>
2017-04-28 22:35:47 -0700

    Rotate hallway so that it lines up with Dwinelle vectors

--- dwinelle/web/js/spaces.js
+++ dwinelle/web/js/spaces.js
@@ -2,14 +2,14 @@ function hallwayType1(length) {
   var space = new THREE.Group();
   space.add(makeArrowHelper(0,0,0,0,1,0));
   // Lighting
-  for (var i = -length/2; i <= length/2; i+=5) {
-    space.add(makePointLight(0,i,2.5, space));
+  for (var i = -length/2; i <= length/2; i+=10) {
+    space.add(makePointLight(i,0,2.5, space));
   }
   // Floor
-  space.add(makePlane(5  , length, 0   , 0   , 0 , null, null      , null, floorMat));
+  space.add(makePlane(5  , length, 0, 0   , 0, null, null, Math.PI/2, floorMat));
   // Left Wall
-  space.add(makePlane(2.5, length, 2.5 , 0, 1.25, null, Math.PI/2 , null, paintMat));
+  space.add(makePlane(2.5, length, 0,  2.5, 1.25, Math.PI/2, null, Math.PI/2, paintMat));
   // Right Wall
-  space.add(makePlane(2.5, length, -2.5, 0, 1.25, null, -Math.PI/2, null, paintMat));
+  space.add(makePlane(2.5, length, 0, -2.5, 1.25, -Math.PI/2, null, Math.PI/2, paintMat));
   return space;
 }
