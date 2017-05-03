027918b54c02006ab423d3ec45da8ef9337b4146
Ollie O'Donnell <oliverodaa@gmail.com>
2017-05-02 23:21:55 -0700

    first-person camera toggle

--- dwinelle/web/js/camera_helpers.js
+++ dwinelle/web/js/camera_helpers.js
@@ -1,3 +1,7 @@
+var WALKING_SPEED_RATIO = 30; // how many times faster than walking speed are you?
+var FIRST_PERSON = false;
+var RESET_CAMERA_POSITION = function() {camera.position.set(-300, 180, 180);}
+
 function nextCameraTween(path, index, sf, ef) {
     var start = convertVec(coords[path[index]]);
     var end = convertVec(coords[path[index+1]]);
@@ -13,5 +17,17 @@ function nextCameraTween(path, index, sf, ef) {
         return tween;
     } else {
         return tween.chain(nextCameraTween(path,index+1, sf, ef));
+    }
+}
+
+function toggleCamera() {
+    if (FIRST_PERSON) {
+        console.log("switch to 3rd person");
+        RESET_CAMERA_POSITION();
+        controls.maxDistance = 100;
+        FIRST_PERSON = false;
+    } else {
+        controls.maxDistance = 1;
+        FIRST_PERSON = true;
     }
 }
